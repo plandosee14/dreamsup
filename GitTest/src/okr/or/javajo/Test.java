790b8e66a9dbe538d810aa3601dd22d14a6b4b28
Cys <rcn115@naver.com>
2016-10-15 05:03:52 +0900

    View file list_2  - 기능별 폴더 구조화

--- GitTest/src/okr/or/javajo/Test.java
+++
@@ -1,9 +0,0 @@
-package okr.or.javajo;
-
-public class Test {
-	public void haeun(){
-		System.out.println("bless me");
-		System.out.println("대맹아 테트리스 그만해라");
-		System.out.println("온몸의 뼈를 테트리스처럼 맞춰버릴라");
-	}
-}
